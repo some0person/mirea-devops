f5caccb2478e51dc6aa0b4efe2359893f3f35b5c
some0person <109872677+some0person@users.noreply.github.com>
2024-11-14 10:04:28 +0300

    Tried to fix Django migration

--- django/website/settings.py
+++ django/website/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": "devops",
-        "USER": environ.get("POSTGRES_USER"),
-        "PASSWORD": environ.get("POSTGRES_PASSWORD"),
+        "USER": "devops",
+        "PASSWORD": "devops",
         "HOST": "db",
         "PORT": 5432,
         
